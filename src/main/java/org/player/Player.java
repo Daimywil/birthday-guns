8e437622e39d096317824dafd396e1b4ddc1faf5
ceylindiepeveen <cedricdiepeveen@gmail.com>
2025-03-24 20:24:31 +0100

    added comments

--- src/main/java/org/player/Player.java
+++ src/main/java/org/player/Player.java
@@ -2,29 +2,45 @@ package org.player;
 
 import com.github.hanyaeger.api.Coordinate2D;
 import com.github.hanyaeger.api.entities.Direction;
+import com.github.hanyaeger.api.entities.Newtonian;
 import com.github.hanyaeger.api.entities.impl.DynamicSpriteEntity;
 import com.github.hanyaeger.api.userinput.KeyListener;
 import javafx.scene.input.KeyCode;
 
 import java.util.Set;
 
-public class Player extends DynamicSpriteEntity implements KeyListener {
+public class Player extends DynamicSpriteEntity implements KeyListener, Newtonian {
     public Player(String resource, Coordinate2D initialLocation) {
         super(resource, initialLocation);
+
+        setFrictionConstant(0.1);
+        setGravityConstant(0);
+        setGravityDirection(0);
     }
 
     @Override
     public void onPressedKeysChange(Set<KeyCode> pressedKeys) {
+        int xSpeed = 0;
+        int ySpeed = 0;
+
+        //checkt toetsen en zet soort positie
         if (pressedKeys.contains(KeyCode.A)) {
-            setCurrentFrameIndex(0);
-            setMotion(3, Direction.LEFT);
-        } else if (pressedKeys.contains(KeyCode.D)) {
-            setCurrentFrameIndex(1);
-            setMotion(3, Direction.RIGHT);
-        } else if (pressedKeys.contains(KeyCode.W)) {
-            setMotion(3, Direction.UP);
-        } else if (pressedKeys.contains(KeyCode.S)) {
-            setMotion(3, Direction.DOWN);
+            xSpeed = -1;
+        }
+        if (pressedKeys.contains(KeyCode.D)) {
+            xSpeed = 1;
+        }
+        if (pressedKeys.contains(KeyCode.W)) {
+            ySpeed = -1;
+        }
+        if (pressedKeys.contains(KeyCode.S)) {
+            ySpeed = 1;
+        }
+
+        //maakt een hoek van die positie en zet de beweging in gang
+        if (xSpeed != 0 || ySpeed != 0) {
+            double direction = Math.toDegrees(Math.atan2(xSpeed, ySpeed)); //Berekend de richting
+            setMotion(3, direction);
         }
     }
 }
