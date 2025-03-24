8e72b10cc0601888b941c9c4b43840b428d40876
Daimywil <daimywillemsen@hotmail.nl>
2025-03-24 14:35:23 +0100

    Init

---
+++ src/main/java/module-info.java
@@ -0,0 +1,5 @@
+module BirthdayGuns {
+    requires hanyaeger;
+
+    exports org;
+}
---
+++ src/main/java/org/BirthdayGuns.java
@@ -0,0 +1,21 @@
+package org;
+
+import com.github.hanyaeger.api.YaegerGame;
+
+public class BirthdayGuns extends YaegerGame
+{
+    public static void main( String[] args )
+    {
+        launch(args);
+    }
+
+    @Override
+    public void setupGame() {
+
+    }
+
+    @Override
+    public void setupScenes() {
+
+    }
+}
