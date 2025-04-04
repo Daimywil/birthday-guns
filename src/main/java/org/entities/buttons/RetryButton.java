44ac80cae5e79eb934ff60ebad71d12a2d90763e
ceylindiepeveen <cedricdiepeveen@gmail.com>
2025-04-04 10:41:03 +0200

    Eindscherm

--- src/main/java/org/entities/buttons/RetryButton.java
+++ src/main/java/org/entities/buttons/RetryButton.java
@@ -2,7 +2,6 @@ package org.entities.buttons;
 
 import com.github.hanyaeger.api.AnchorPoint;
 import com.github.hanyaeger.api.Coordinate2D;
-import com.github.hanyaeger.api.entities.impl.RectangleEntity;
 import com.github.hanyaeger.api.entities.impl.TextEntity;
 import com.github.hanyaeger.api.userinput.MouseButtonReleasedListener;
 import javafx.scene.input.MouseButton;
@@ -11,15 +10,21 @@ import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
 import org.BirthdayGuns;
 
-public class ButtonBackdrop extends RectangleEntity {
+public class RetryButton extends TextEntity implements MouseButtonReleasedListener {
     private BirthdayGuns birthdayGuns;
 
-    public ButtonBackdrop(Coordinate2D initialLocation, BirthdayGuns birthdayGuns) {
+    public RetryButton(Coordinate2D initialLocation, BirthdayGuns birthdayGuns) {
         super(initialLocation);
         this.birthdayGuns = birthdayGuns;
+        setText("PLAY AGAIN");
+        setFont(Font.font("Roboto", FontWeight.BOLD, 64));
         setAnchorPoint(AnchorPoint.TOP_CENTER);
-        setFill(Color.rgb(255, 255, 0));
-        setWidth(512);
-        setHeight(96);
+        setFill(Color.rgb(0, 0, 0));
+        setViewOrder(10);
+    }
+
+    @Override
+    public void onMouseButtonReleased(MouseButton button, Coordinate2D coordinate2d) {
+        birthdayGuns.setActiveScene(0);
     }
 }
--- src/main/java/org/scenes/ScoreScene.java
+++ src/main/java/org/scenes/ScoreScene.java
@@ -1,7 +1,10 @@
 package org.scenes;
 
+import com.github.hanyaeger.api.Coordinate2D;
 import com.github.hanyaeger.api.scenes.StaticScene;
 import org.BirthdayGuns;
+import org.entities.buttons.*;
+import org.statistics.GameStatistics;
 
 public class ScoreScene extends StaticScene {
     private BirthdayGuns birthdayGuns;
@@ -17,6 +20,24 @@ public class ScoreScene extends StaticScene {
 
     @Override
     public void setupEntities() {
+        var gameOverText = new TextLabel(new Coordinate2D(getWidth() * 0.5, getHeight() * 0.5), birthdayGuns);
+        gameOverText.setText("Game Over!");
+        addEntity(gameOverText);
+
+        var retryButton = new RetryButton(new Coordinate2D(getWidth() * 0.5, getHeight() * 0.8), birthdayGuns);
+        addEntity(retryButton);
+        var retryButtonBackdrop = new ButtonBackdrop(new Coordinate2D(getWidth() * 0.5, getHeight() * 0.8), birthdayGuns);
+        addEntity(retryButtonBackdrop);
+
+        GameStatistics gameStatistics = birthdayGuns.getGameStatistics();
+
+        var scoreText = new TextLabel(new Coordinate2D(getWidth() * 0.5, getHeight() * 0.1), birthdayGuns);
+        double score = gameStatistics.getScore();
+        double kills = gameStatistics.getKills();
+        double time = gameStatistics.getTimeAlive();
+
+        scoreText.setText("SCORE: " + score + "\n" + "KILLS: " + kills + "\n" + "TIME: " + time);
+        addEntity(scoreText);
 
     }
 }
