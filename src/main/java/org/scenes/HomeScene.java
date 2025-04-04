c069e2b36e3f00b6a31b723498e59721ffce1ad0
Daimywil <daimywillemsen@hotmail.nl>
2025-04-04 10:57:33 +0200

    added current map label

--- src/main/java/org/scenes/HomeScene.java
+++ src/main/java/org/scenes/HomeScene.java
@@ -5,6 +5,7 @@ import com.github.hanyaeger.api.scenes.StaticScene;
 import org.BirthdayGuns;
 import org.entities.buttons.MapButton;
 import org.entities.buttons.StartButton;
+import org.entities.buttons.TextLabel;
 import org.entities.buttons.ButtonBackdrop;
 
 public class HomeScene extends StaticScene {
@@ -21,15 +22,19 @@ public class HomeScene extends StaticScene {
 
     @Override
     public void setupEntities() {
+        var gameOverText = new TextLabel(new Coordinate2D(getWidth() * 0.5, getHeight() * 0.2), birthdayGuns);
+        gameOverText.setText("Current map: " + (GameScene.mapNumber == 0 ? "The Backyard" : "The Dungeon"));
+        addEntity(gameOverText);
+
         var startButton = new StartButton(new Coordinate2D(getWidth() * 0.5, getHeight() * 0.8), birthdayGuns);
         addEntity(startButton);
         var startButtonBackdrop = new ButtonBackdrop(new Coordinate2D(getWidth() * 0.5, getHeight() * 0.8), birthdayGuns);
         addEntity(startButtonBackdrop);
 
-        var theBackyardButton = new MapButton("sprites/maps/icons/TheBackyardMap.png", new Coordinate2D(getWidth() * 0.3, getHeight() * 0.5), birthdayGuns, 0);
+        var theBackyardButton = new MapButton("sprites/maps/icons/TheBackyardMap.png", new Coordinate2D(getWidth() * 0.3, getHeight() * 0.5), birthdayGuns, 0, gameOverText, "The Backyard");
         addEntity(theBackyardButton);
 
-        var theDungeonButton = new MapButton("sprites/maps/icons/TheDungeonMap.png", new Coordinate2D(getWidth() * 0.7, getHeight() * 0.5), birthdayGuns, 1);
+        var theDungeonButton = new MapButton("sprites/maps/icons/TheDungeonMap.png", new Coordinate2D(getWidth() * 0.7, getHeight() * 0.5), birthdayGuns, 1, gameOverText, "The Dungeon");
         addEntity(theDungeonButton);
     }
 }
